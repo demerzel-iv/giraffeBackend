f6f96ef8aa4399dae86c7c78fdece7520f6373f6
demerzel-iv <demerzel_iv@qq.com>
2021-09-03 12:27:05 +0800

    修复了problemList中，中文符号导致的错误

--- src/main/java/com/giraffe/restservice/service/ProblemService.java
+++ src/main/java/com/giraffe/restservice/service/ProblemService.java
@@ -23,24 +23,30 @@ public class ProblemService {
     private HashMap<String, Object> processQBody(String qBody) {
         final String opts = "ABCDEF";
 
-        HashMap<String, Object> ret = new HashMap<>();
-        ArrayList<Integer> dividePos = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            int ind = qBody.indexOf(opts.substring(i, i + 1) + ".");
-            if (ind != -1) {
-                dividePos.add(ind);
+        try {
+            qBody = qBody.replace("．", ".");
+            HashMap<String, Object> ret = new HashMap<>();
+            ArrayList<Integer> dividePos = new ArrayList<>();
+            for (int i = 0; i < 6; i++) {
+                int ind = qBody.indexOf(opts.substring(i, i + 1) + ".");
+                if (ind != -1) {
+                    dividePos.add(ind);
+                }
             }
-        }
 
-        ArrayList<String> optionList = new ArrayList<>();
-        ret.put("queryBody", qBody.substring(0, dividePos.get(0)));
-        for (int i = 0; i < dividePos.size() - 1; i++) {
-            optionList.add(qBody.substring(dividePos.get(i), dividePos.get(i+1)));
-        }
-        optionList.add(qBody.substring(dividePos.get(dividePos.size() - 1)));
-        ret.put("options", optionList);
+            ArrayList<String> optionList = new ArrayList<>();
+            ret.put("queryBody", qBody.substring(0, dividePos.get(0)));
+            for (int i = 0; i < dividePos.size() - 1; i++) {
+                optionList.add(qBody.substring(dividePos.get(i), dividePos.get(i + 1)));
+            }
+            optionList.add(qBody.substring(dividePos.get(dividePos.size() - 1)));
+            ret.put("options", optionList);
 
-        return ret;
+            return ret;
+        } catch (Exception e) {
+            System.err.println("processing qBody error : " + qBody);
+            return null;
+        }
     }
 
     public ArrayList<Object> getProblemList(String label) throws NetworkRequestFailedException {
@@ -48,6 +54,9 @@ public class ProblemService {
         JsonNode tree = jsonService.readTree(networkService.problemList(label)).get("data");
         for (int i = 0; i < tree.size(); i++) {
             HashMap<String, Object> problem = processQBody(tree.get(i).get("qBody").asText());
+            if (problem == null) {
+                continue;
+            }
             problem.put("answer", tree.get(i).get("qAnswer").asText());
             list.add(problem);
         }
